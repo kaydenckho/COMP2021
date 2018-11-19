48e27d7d316f1d2bc9e7c1819d9e6a3678a8f719
TheRealzz <yanhowanga32@gmail.com>
2018-11-19 16:57:22 +0800

    public Cell valid_move(dir, x, y) added
    The first version of valid_move method.

--- Jungle game/src/hk/edu/polyu/comp/comp2021/jungle/model/Animals.java
+++ Jungle game/src/hk/edu/polyu/comp/comp2021/jungle/model/Animals.java
@@ -34,34 +34,34 @@ enum Animals {
         }
     }
 
-    public boolean valid_move(int dir, int x, int y){                                           //dir determines up, down, left, right: cell row and column x,y
-        //if(!boardClassname.arrayName[x][y].hasAnimal()){                                      // Check if the cell move from has an animal (Should be done in board class)
-        //    return false;                                                                     // valid_move should be called in board class after interpreting the move command
-        //}
-        switch(dir){                                                                            // Check if the moving direction is out of bound
+    public Cell valid_move(int dir, int x, int y){                                           //dir determines up, down, left, right: cell row and column x,y (dir checked in board class)
+        //if(!boardClassname.arrayName[x][y].hasAnimal()){                                   // Check if the cell move from has an animal (Should be done in board class)
+        //    return false;                                                                  // valid_move should be called in board class after interpreting the move command
+        //}                                                                                  // if returned address not null || address = destination address(done in board class) -> next step: move.
+        switch(dir){                                                                         // Check if the moving direction is out of bound
             case 1:{
-                if(y+1 > 8){return false;}
+                if(y+1 > 8){return null;}
                 else{
                     y=y+1;
                     Cell nextcell = boardClassname.arrayName[x][y]
                 }
             }
             case 2:{
-                if(y-1 < 0){return false;}
+                if(y-1 < 0){return null;}
                 else{
                     y=y-1
                     Cell nextcell = boardClassname.arrayName[x][y]
                 }
             }
             case 3:{
-                if(x-1 < 0){return false;}
+                if(x-1 < 0){return null;}
                 else{
                     x=x-1;
                     Cell nextcell = boardClassname.arrayName[x][y]
                 }
             }
             case 4:{
-                if(x+1 > 6){return false;}
+                if(x+1 > 6){return null;}
                 else{
                     x = x+1;
                     Cell nextcell = boardClassname.arrayName[x][y]
@@ -72,15 +72,15 @@ enum Animals {
             if(this.rank==7 || this.rank ==6 || this.rank==1){
                 if(this.rank==1){                                   // mouse goes into water
                     if(nextcell.hasAnimal()){                       // check if there's already animal in water
-                        return false;                               // move is blocked by another mouse
+                        return null;                               // move is blocked by another mouse
                     }
                     else{
-                        return true;                                // valid if no animal in water
+                        return nextcell;                                // valid if no animal in water
                     }
                 }
                 else{                                               // Lion and tiger jump across river
                     if(nextcell.hasAnimal()){                       // check if there's already animal in water
-                        return false;                               // move is blocked by the animal
+                        return null;                               // move is blocked by the animal
                     }
                     else{
                         return valid_move(dir, x, y);                   // if the next cell is still water
@@ -88,12 +88,12 @@ enum Animals {
                 }
             }
             else{
-                return false;                                       // For other animals, no one can go into the river
+                return null;                                       // For other animals, no one can go into the river
             }
         }
         else{
-            nextcell not river
-        }
-    }
-
+            return nextcell;                                            // if next cell is not water, then all animals can attempt eat function called in board class
+        }                                                           // including case: mouse(water) -> mouse, animal -> animal(trap), animal(trap) -> animal
+    }                                                               // if next cell has animal -> attempt eat (attacker not in water && higherRank || attacker in trap )
+                                                                    // call move if valid_move and destination matched, call eat in move if destination has an animal
 }
